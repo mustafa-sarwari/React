d5c73d40670e2dda864aec79d12b9b83e3933175
mustafa-sarwari <gm.sarwari@outlook.com>
2025-10-28 14:52:00 -0700

    the update start working

--- src/App.js
+++ src/App.js
@@ -6,64 +6,83 @@ function App() {
   const [role, setRole] = useState("Dev");
   const [employees, setEmployees] = useState([
     {
+      id: 1,
       name: "Sarah",
       role:"CEO",
       img:"../../photo/img (1).jpg"
     },
     {
+      id: 2,
       name:"Deedee",
       role:"CTO",
       img:"../../photo/img (2).jpg"
     },
     {
-       name:"Johon",
+      id: 3,
+      name:"Johon",
       role:"OTO",
       img:"../../photo/img (3).jpg"
     },
     {
+      id: 4,
       name: "Ana", 
       role: "Director",
       img:"../../photo/img (4).jpg"
     },
     {
+      id: 5,
       name: "Fatima",
       role:"General Manager",
       img:"../../photo/img (1).webp"
     },
     {
+      id: 6,
       name: "Martin",
       role: "General Product Manger",
       img: "../../photo/img (2).webp"
     },
     {
+      id: 7,
       name: "Ellie",
       role:"General Operation Manager", 
       img:"../../photo/img (3).webp"
     }
 
     ]);
+
+  function updateEmployee(id, newName, newRole){
+    const updatedEmployee = employees.map((employee) => {
+      if(id == employee.id) {
+        return  {...employee, name: newName, role: newRole};
+      }
+      return employee;
+    });
+    setEmployees(updatedEmployee);
+  }
+
+
   const showEmployees = true;
 
   return (
     <div className="App ">
-      {console.log("inside the return")}
       {
        showEmployees ? (
       <>
          <input type="text" onChange={ (e) => {
-            console.log(e.target.value);
             setRole(e.target.value);
             }} 
           />
           <div className='flex flex-wrap justify-center'>
             {
-            employees.map((employees) => {
-              console.log(uuidv4())
-              return (<Employee
-              key = {uuidv4()}
-              name = {employees.name} 
-              role = {employees.role}
-              img = {employees.img}
+            employees.map((employee) => {
+              return (
+              <Employee
+              key = {employee.id}
+              id= {employee.id}
+              name = {employee.name} 
+              role = {employee.role}
+              img = {employee.img}
+              updateEmployee = {updateEmployee}
               /> );
             })
             }
--- src/component/EditEmployee.js
+++ src/component/EditEmployee.js
@@ -2,7 +2,9 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-function EditEmployee() {
+function EditEmployee(props) {
+  const [name, setName] = useState(props.name);
+  const [role, setRole] = useState(props.role);
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
@@ -22,7 +24,14 @@ function EditEmployee() {
           <Modal.Title>Update Employee</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <form id = "editmodel" className="w-full max-w-sm">
+          <form 
+          onSubmit={(e) => {
+            e.preventDefault();
+            props.updateEmployee(props.id, name, role);
+          }}
+          id = "editmodel" 
+          className="w-full max-w-sm">
+
                 <div className="md:flex md:items-center mb-6">
                     <div className="md:w-1/3">
                         <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" 
@@ -33,7 +42,11 @@ function EditEmployee() {
                         <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" 
                         id="name" 
                         type="text" 
-                        value="Jane Doe" />
+                        value={name}
+                        onChange = {(e) => {
+                          setName(e.target.value);
+                        }}
+                        />
                     </div>
                 </div>
 
@@ -47,7 +60,12 @@ function EditEmployee() {
                         <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" 
                         id="role" 
                         type="text" 
-                        value="Jane Doe" />
+                        value={role}
+                        onChange = {(e) => {
+                          setRole(e.target.value);
+                        }}
+                  
+                        />
                     </div>
                 </div>
             </form>
@@ -56,7 +74,8 @@ function EditEmployee() {
           <button className ="bg-slate-400 hover:bg-slate-500 text-white font-bold py-2 px-4 rounded" 
           onClick={handleClose}>Close</button>
 
-          <button className ="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded" 
+          <button 
+          className ="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded" 
           form ="editmodel">Update</button>
         </Modal.Footer>
       </Modal>
